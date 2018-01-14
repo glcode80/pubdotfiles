f2285db05af3418452d49c9636dad2e635c88c99
Meinrad Hunold <coding@hunold.ch>
2018-01-14 17:25:59 +0100

    processes and logging

--- cmd_linux.sql
+++ cmd_linux.sql
@@ -28,13 +28,59 @@ enter-enter to exit
 
 
 *** Prozesssteuerung -> https://wiki.ubuntuusers.de/Shell/Prozesssteuerung/
-ctrl-z = send job to background
+ctrl-z = send job to background and stop it
+bg %x = continue process runinng in background
 jobs = show all jobs
 command & = start it directly in background!
 fg %x = get job back to foreground
 bg %x = send to background
 disown -h %x => job läuft weiter, wenn terminal beendet wurde!!
-nohup command & => job direkt schon laufen lassen im hintergrund, ohne dass er beendet wird
+nohup command & => (nicht so gut, besser screen!) job direkt schon laufen lassen im hintergrund, ohne dass er beendet wird
+
+
+** send running process to screen
+a) send it to background
+ctrl-z
+b) resume in background
+bg
+c) make job continue when terminal ends
+disown %1
+d) start and screen and take over the job
+screen -S 1
+find pid, two options (first one is the pid):
+ pgrep [command]
+ ps aux | grep [command]
+take over pid: 
+ reptyr [pid]
+e) detach/restart screen
+ctrl-a d
+screen -r
+
+
+** Output to textfile / console / logfile / stdout
+output to new file: >
+append to file: >>
+!!! no buffer in pyhton: -u (call with python3 instead of executing the file)
+
+**TOP1* Everything to outfile, including error (nothing to stdout):
+python3 -u test.py > outfile.txt 2>&1
+
+vim refresh file (or use tail logfile.txt):
+:e
+
+**TOP2* Everything also to the stdout - replace file
+python3 -u test.py 2>&1 | tee outfile.txt
+
+Everything also to the stdout - append file
+python3 -u test.py 2>&1 | tee -a outfile.txt
+
+* -> spcial with process:
+Run it in background as job (not in screen) and send to append file:
+python3 -u test.py > outfile.txt &
+python3 -u test.py > outfile.txt 2>&1 &
+
+
+
 
 
 *** Updates (at least monthly)
@@ -94,6 +140,13 @@ dpkg -l [find all installed packages]
 * remove directory including everything
 rm -rf xxxxx
 
+* create smybolic link
+- set symoblic link -> this places "sourcefolder" into "targetfolder"
+  ln -s /PATH_FOR_ACTUAL_DATA/sourcefolder /PATH_TO_LINK_TO/targetfolder
+
+- delete symbolic link
+  rm sourcefolder
+
 
 *** remove unused network-facing services
 sudo netstat -tulpn
@@ -142,6 +195,17 @@ cat access.log | grep 'GET / ' | awk '{print $1}' |  sort -n | uniq -c | sort -r
 whole line for root access to output file
 cat access.log | grep 'GET / ' > simplelogs.txt
 
+*** speed test
+Write speed:
+sync; dd if=/dev/zero of=~/test.tmp bs=500K count=1024
+Read speed:
+sync; echo 3 | sudo tee /proc/sys/vm/drop_caches
+sync; time dd if=~/test.tmp of=/dev/null bs=500K count=1024
+rm ~/test.tmp
 
+sudo apt-get iozone3
+
+iozone -e -I -a -s 100M -r 4k -i 0 -i 1 -i 2 
+[-f /path/to/file]
 
 
