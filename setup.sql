20734d1baf14dfd1ab1723b71b51e6aded1d5145
Meinrad Hunold <coding@hunold.ch>
2018-09-10 08:56:13 +0200

    install tld

--- setup.sql
+++ setup.sql
@@ -67,6 +67,7 @@ sudo pip3 install pytz
 sudo pip3 install flake8
 sudo pip3 install autopep8
 sudo pip3 install pylint
+sudo pip3 install tld
 sudo pip3 install matplotlib
 sudo pip3 install numpy
 
