355e64ae4513f5f36fafff4d28ffe6e4e7b3bac5
Meinrad Hunold <coding@hunold.ch>
2021-01-29 12:45:43 +0100

    clean up phpbenchmark

--- phpbench.php
+++ phpbench.php
@@ -54,13 +54,13 @@
 	$total = 0;
 	$functions = get_defined_functions();
 	$line = str_pad("-",38,"-");
-	echo "<pre>$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)."|\n$line\nStart : ".date("Y-m-d H:i:s")."\nServer : {$_SERVER['SERVER_NAME']}@{$_SERVER['SERVER_ADDR']}\nPHP version : ".PHP_VERSION."\nPlatform : ".PHP_OS. "\n$line\n";
+	echo "\n$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)."|\n$line\nStart : ".date("Y-m-d H:i:s")."\nPHP version : ".PHP_VERSION."\nPlatform : ".PHP_OS. "\n$line\n";
 	foreach ($functions['user'] as $user) {
 		if (preg_match('/^test_/', $user)) {
 			$total += $result = $user();
             echo str_pad($user, 25) . " : " . $result ." sec.\n";
         }
 	}
-	echo str_pad("-", 38, "-") . "\n" . str_pad("Total time:", 25) . " : " . $total ." sec.</pre>";
+	echo str_pad("-", 38, "-") . "\n" . str_pad("Total time:", 25) . " : " . $total ." sec.\n\n";
 	
 ?>
